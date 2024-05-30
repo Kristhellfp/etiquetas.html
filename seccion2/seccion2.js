522850426d880ce4c76fc4a79060ebd22b33e516
Andrx <aamelendez@scl.edu.gt>
2024-05-30 11:10:55 -0600

    correpcion de estructura

--- seccion2/seccion2.js
+++ seccion2/seccion2.js
@@ -4,27 +4,18 @@ seccion2_DOM.innerHTML =`
 <section class="seccion2">
     <table class="table2">
             <tr>
-                <th>Nombre</th>
-                <th>Edad</th>
-                <th>Ciudad</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Juan</td>
-                <td>28</td>
-                <td>Madrid</td>
+                <th>header</th>
+                <td>se utiliza para representar un grupo de ayudas introductorias o de navegación. Puede contener uno o más encabezados, logotipos, íconos de navegación, formularios de búsqueda, etc.</td>
             </tr>
             <tr>
-                <td>Ana</td>
-                <td>22</td>
-                <td>Barcelona</td>
+                <th>section</th>
+                <td>El elemento  se utiliza para agrupar contenido temáticamente relacionado. Cada  debe ser un bloque lógico independiente que tenga sentido por sí mismo. Es ideal para dividir el contenido en secciones con un propósito específico.</td>
             </tr>
             <tr>
-                <td>Pedro</td>
-                <td>35</td>
-                <td>Valencia</td>
+                <th>Footer</th>
+                <td>El elemento se utiliza para definir el pie de página de un documento o de una sección. Generalmente, contiene información sobre el autor del documento, enlaces a términos de uso, políticas de privacidad, información de contacto, etc.</td>
             </tr>
+            
     </table>
 </section>
 `;
