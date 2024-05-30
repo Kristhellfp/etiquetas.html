66f3a2e956cface8780db630601508500b5cffc1
Andrx <aamelendez@scl.edu.gt>
2024-05-30 11:03:26 -0600

    section 2 terminada

--- seccion2/seccion2.js
+++ seccion2/seccion2.js
@@ -1,3 +1,30 @@
 let seccion2_DOM = document.querySelector(".seccion2");
 
-seccion2_DOM.innerHTML ="Hola soy Sección 1";
+seccion2_DOM.innerHTML =`
+<section class="seccion2">
+    <table class="table2">
+            <tr>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>Ciudad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Juan</td>
+                <td>28</td>
+                <td>Madrid</td>
+            </tr>
+            <tr>
+                <td>Ana</td>
+                <td>22</td>
+                <td>Barcelona</td>
+            </tr>
+            <tr>
+                <td>Pedro</td>
+                <td>35</td>
+                <td>Valencia</td>
+            </tr>
+    </table>
+</section>
+`;
