45fcea057073ae47247eb58fd486b21e3dd501eb
Iskrisss <klfpaz@scl.edu.gt>
2024-05-30 18:00:49 -0600

    Header y footer

--- header/header.js
+++ header/header.js
@@ -1,3 +1,2 @@
 let header_DOM = document.querySelector(".header");
-
-header_DOM.innerHTML = ' hola soy header';
+header_DOM.innerHTML = ' <header> <h1>Gramática HTML</h1></header> <a href="" class="btn-github">Github</a>';
